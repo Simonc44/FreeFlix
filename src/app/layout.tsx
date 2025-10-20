530a90e8c821463e69274b99fdb8002fcd86ae4d
Simon Chusseau <wehipi8051@leabro.com>
2025-10-20 18:32:58 +0000

    je veux ce svg en logo <svg xmlns="http://www.w3.org/2000/svg" width="24

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="fr" className="dark" suppressHydrationWarning>
       <head>
         <meta name="google" content="notranslate" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='hsl(24 95% 53%)' stroke='hsl(0 0% 8%)' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'><path d='M22 8l-6 6 6 6V8z'/><path d='M14 4h-2a2 2 0 00-2 2v12a2 2 0 002 2h2'/><path d='M4 4h2a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2z'/></svg>" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='hsl(24 95% 53%)' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M20.2 6 3 11l-.9-2.4c-.3-1.1.3-2.2 1.3-2.5l13.5-4c1.1-.3 2.2.3 2.5 1.3Z'></path><path d='m6.2 5.3 3.1 3.9'></path><path d='m12.4 3.4 3.1 4'></path><path d='M3 11h18v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Z'></path></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Bebas+Neue&display=swap" rel="stylesheet" />
