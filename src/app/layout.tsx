abfec13cc51b4b91a3a0e397867c6d228acc8bb8
Simon Chusseau <wehipi8051@leabro.com>
2025-10-20 18:32:17 +0000

    je veux un beau svg qui soit comme le premier emoki mais en moderne

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="fr" className="dark" suppressHydrationWarning>
       <head>
         <meta name="google" content="notranslate" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='hsl(24 95% 53%)' stroke='hsl(0 0% 8%)' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'><path d='M4 11v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-8'/><path d='m4 11 8-8 8 8'/><path d='M4 4v.01'/><path d='M8 4v.01'/><path d='M12 4v.01'/><path d='M16 4v.01'/></svg>" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='hsl(24 95% 53%)' stroke='hsl(0 0% 8%)' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'><path d='M22 8l-6 6 6 6V8z'/><path d='M14 4h-2a2 2 0 00-2 2v12a2 2 0 002 2h2'/><path d='M4 4h2a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2z'/></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Bebas+Neue&display=swap" rel="stylesheet" />
