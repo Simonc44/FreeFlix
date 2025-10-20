60194e0af2c1ca9daf76e171db92200380504e25
Simon Chusseau <wehipi8051@leabro.com>
2025-10-20 18:31:04 +0000

    chnage l'emoji par un beau svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="fr" className="dark" suppressHydrationWarning>
       <head>
         <meta name="google" content="notranslate" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎬</text></svg>" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='hsl(24 95% 53%)' stroke='hsl(0 0% 8%)' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'><path d='M4 11v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-8'/><path d='m4 11 8-8 8 8'/><path d='M4 4v.01'/><path d='M8 4v.01'/><path d='M12 4v.01'/><path d='M16 4v.01'/></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Bebas+Neue&display=swap" rel="stylesheet" />
