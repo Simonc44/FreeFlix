fcdbb5f9d5972c314f51e8fba31210addb5bfee9
Simon Chusseau <wehipi8051@leabro.com>
2025-10-21 08:40:41 +0000

    note mon site sur 10

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -76,7 +76,7 @@ export default function Header() {
               />
             </div>
           </form>
-          {user && (
+          {user ? (
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="relative h-8 w-8 rounded-full">
@@ -101,6 +101,11 @@ export default function Header() {
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
+          ) : (
+            <Button onClick={login}>
+              <UserIcon className="mr-2 h-4 w-4" />
+              Connexion
+            </Button>
           )}
         </div>
       </div>
