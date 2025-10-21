490a8c35d2b731ef7e3867df2f32ddc309230e3f
Simon Chusseau <wehipi8051@leabro.com>
2025-10-21 09:08:58 +0000

    6000-firebase-studio-1760972340553.cluster-64pjnskmlbaxowh5lzq6i7v4ra.cl

--- next.config.ts
+++ next.config.ts
@@ -15,10 +15,6 @@ const nextConfig: NextConfig = {
         source: '/:path*',
         headers: [
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
             key: 'Content-Security-Policy',
             value: "frame-ancestors 'self' https://*.cloudworkstations.dev;",
           },
